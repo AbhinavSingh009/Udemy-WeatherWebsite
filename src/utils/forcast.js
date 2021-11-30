1a7fecc730ac8718e64a041f6d05cb7a7650cf91
AbhinavSingh009 <abhinav.singh1009@gmail.com>
2021-11-29 21:14:53 -0500

    Added Humidity to the forcast

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -17,7 +17,8 @@ const forcast = (latitude, longitude, callback) => {
         }
 
         callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature +
-            '. It feels like ' + body.current.feelslike + ' outside. ');
+            '. It feels like ' + body.current.feelslike + ' outside. ' +
+            'The Humidity is ' + body.current.humidity + '%');
 
     });
 }
